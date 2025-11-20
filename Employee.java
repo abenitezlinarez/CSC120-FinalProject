d99e11d04fd93b71bcd786a4327bff9f9c1950b9
Michelle Jiang <mhjiang@smith.edu>
2025-11-20 09:55:26 -0500

    added methods

--- Employee.java
+++ Employee.java
@@ -25,9 +25,43 @@ public class Employee {
         this.user = user; 
     }
 
+    /**
+     * Initial user greeting 
+     */
     public void greetCustomer() { 
         System.out.println("Hello" + this.user + ", welcome to " + this.store + "! ");
+    }
+
+    /**
+     * Asks user to buy items from inventory 
+     */
+    public void mainConversation() { 
         System.out.println("What would you like to purchase from this store? ");
+        String item = getInput(); 
+        // check if item is in shopping list 
+        // if item in this.store.items key?? 
+        // buy the item 
+        // else suggest item 
+    }
+
+    /**
+     * Gets input from user & converts it to a string
+     * @return string trimmed user input 
+     */
+    public String getInput() { 
+        Scanner input = new Scanner(System.in);  
+        input.close();
+        String string = input.toString();
+        string = string.trim(); 
+        return string; 
+    }
+
+    /** 
+     * Given the desired item of the user, suggests the next item in the inventory -- OR suggests similar item
+     */
+    public void suggestItem(String item) { 
+        // suggests item
+        // adds item to shopping list!
     }
 
     public static void main(String[] args) {
